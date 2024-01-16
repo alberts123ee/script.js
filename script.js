187c3dae0c4b046b5b3de0c370e600e8edaa1122
alberts123ee <128698486+alberts123ee@users.noreply.github.com>
2024-01-16 08:46:06 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log(123)
+
+var mainigais = 1
+let mainigais2 = 2
+const mainigais3 = 3
+console.log(mainigais3)
+const vards = "Alberts"
+const vecums = 15
+
+console.log(vards, vecums)
+console.log(3 * 2)
+console.log(vards + 2)
+console.log(typeof vards)
+console.log(typeof 2)
